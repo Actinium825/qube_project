30f9da96f83157fb0acc509ecd1827d0d9b49734
Actinium825 <cruz.d.ace@proton.me>
2025-06-02 00:12:29 +0800

    chore: update packages
    - set app state and qube details to abstract class due to updated freezed
    - set qube details form to sealed class due to updated freezed and remove underscores
    - use pattern matching instead of removed when in step 2 tab vm

--- lib/models/qube_details_form.dart
+++ lib/models/qube_details_form.dart
@@ -3,8 +3,8 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'qube_details_form.freezed.dart';
 
 @freezed
-class QubeDetailsForm with _$QubeDetailsForm {
-  const factory QubeDetailsForm.name(String name) = _Name;
-  const factory QubeDetailsForm.email(String email) = _Email;
-  const factory QubeDetailsForm.phone(String phone) = _Phone;
+sealed class QubeDetailsForm with _$QubeDetailsForm {
+  const factory QubeDetailsForm.name(String name) = Name;
+  const factory QubeDetailsForm.email(String email) = Email;
+  const factory QubeDetailsForm.phone(String phone) = Phone;
 }
